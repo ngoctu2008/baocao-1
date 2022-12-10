e4257c541918ccc824d90aa774cb6948292f0883
Tuan <tuanpb1988@gmail.com>
2022-12-10 22:30:03 +0700

    Update config.php

--- modules/report/admin/config.php
+++ modules/report/admin/config.php
@@ -24,7 +24,7 @@ if ($nv_Request->isset_request('submit', 'post')) {
 		$error[] = $lang_module['require_valid_time'];
 	} else {
 		$time = explode('-', $array_config['valid_time']);
-		if (empty($time) or (0 < $time[0]) or ($time[1] > 24)) {
+		if ((0 < $time[0]) or ($time[1] > 24)) {
 			$error[] = $lang_module['err_valid_time'] . $time[0] . $time[1];
 		}
 		if ($time[0] >= $time[1]) {
